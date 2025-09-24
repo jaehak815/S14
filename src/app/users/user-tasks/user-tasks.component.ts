2df999f543e1dd4d5907715b68b78d5098821aac
Jay <rright815@gmail.com>
2025-09-25 00:20:55 +0800

    275

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,13 +1,14 @@
 import { Component, computed, DestroyRef, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
 import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
 })
 export class UserTasksComponent implements OnInit {
   //userId = input.required<string>();
